feaad1377c01eb127c184cf95a2e0c34438b6528
pnuehrenberg <paul.nuehrenberg@uni.kn>
2022-09-26 18:42:30 +0200

    add visualization functionality

--- src/pyTrajectory/visualization.py
+++ src/pyTrajectory/visualization.py
@@ -1,8 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import collections
+from matplotlib.colors import to_rgb
+from copy import deepcopy
 
 from .trajectory_operations import get_pose_segment_lengths
+
+
+VISUALIZATION_CONFIG = dict(
+    plot_segmentation = True,
+    plot_pose = True,
+    plot_position = False,
+    segmentation_edge_color = 'k',
+    segmentation_edge_alpha = 1,
+    segmentation_face_color = 'k',
+    segmentation_face_alpha = 0,
+    segmentation_edge_width = 0.5,
+    pose_line_color = 'k',
+    pose_line_alpha = 1,
+    pose_line_width = 1,
+    position_marker = 'o',
+    position_face_color = 'k',
+    position_face_alpha = 1,
+    position_edge_color = 'k',
+    position_edge_alpha = 0,
+    position_line_width = 1,
+    position_size = 5
+)
 
 
 def get_trajectory_key(trajectory, keys=None):
@@ -25,56 +49,39 @@ def get_trajectory_range(trajectory):
     return (x_min, x_max), (y_min, y_max)
 
 
-def get_trajectory_alpha_from_density(trajectory):
-    (x_min, x_max), (y_min, y_max) = get_trajectory_range(trajectory)
-    if None in [x_min, x_max, y_min, y_max]:
-        return None
-    key = get_trajectory_key(trajectory, ['segmentation', 'pose', 'position'])
-    if key is None:
-        return None
-    bin_width = 20
-    if trajectory['pose'] is not None:
-        keypoint_indices = np.arange(trajectory['pose'].shape[1])
-        pose_segment_lengths = get_pose_segment_lengths(trajectory,
-                                                        keypoint_indices)
-        mean_pose_length = pose_segment_lengths.sum(axis=1).mean() / 4
-    bins = [np.arange(x_min, x_max + bin_width, bin_width),
-            np.arange(y_min, y_max + bin_width, bin_width)]
-    if np.any(np.asarray([len(bins_d) for bins_d in bins]) > 100):
-        bins = 100
-    bin_counts, bin_edges = np.histogramdd(trajectory[key].reshape(-1, 2),
-                                           bins=bins)
-    positions_digitized = np.transpose([np.digitize(c, bins[1:-1])
-            for c, bins in zip(trajectory[key].reshape(-1, 2).T, bin_edges)])
-    local_counts = bin_counts[positions_digitized[:, 0], positions_digitized[:, 1]]
-    alpha = np.zeros(local_counts.shape) + 0.5
-    alpha[local_counts > 5] = 0.25
-    alpha[local_counts > 10] = 0.1
-    alpha[local_counts > 25] = 0.06
-    alpha[local_counts > 50] = 0.03
-    alpha[local_counts > 100] = 0.02
-    return alpha
+def plot_trajectory(trajectory, ax, visualization_config=None, **visualization_kwargs):
 
+    def get_value(arg):
+        nonlocal visualization_config
+        if arg in visualization_config:
+            return visualization_config[arg]
+        return tuple([*to_rgb(visualization_config[f'{arg}_color']),
+                              visualization_config[f'{arg}_alpha']])
 
-def plot_trajectory(trajectory, ax):
-    # alpha = get_trajectory_alpha_from_density(trajectory)
-    alpha = np.arange(1, len(trajectory))
-    pose_color = 0
-    if trajectory['segmentation'] is not None:
+    visualization_config = visualization_config or deepcopy(VISUALIZATION_CONFIG)
+    for arg, value in visualization_kwargs.items():
+        visualization_config[arg] = value
+
+    if get_value('plot_segmentation') and trajectory['segmentation'] is not None:
         coll_segmentation = collections.PolyCollection(trajectory['segmentation'],
-                                                       edgecolor=[(0, 0, 0, a) for a in alpha],
-                                                       facecolor=[(0, 0, 0, a / 4) for a in alpha],
-                                                       lw=0.25)
+                                                       edgecolor=get_value('segmentation_edge'),
+                                                       facecolor=get_value('segmentation_face'),
+                                                       lw=get_value('segmentation_edge_width'))
         ax.add_collection(coll_segmentation)
-        pose_color = 1
-    if trajectory['pose'] is not None:
+
+    if get_value('plot_pose') and trajectory['pose'] is not None:
         coll_pose = collections.PolyCollection(trajectory['pose'],
                                                closed=False,
-                                               edgecolor=[(pose_color, 0, 0, a) for a in alpha],
+                                               edgecolor=get_value('pose_line'),
                                                facecolor=(0, 0, 0, 0),
-                                               lw=0.5,
+                                               lw=get_value('pose_line_width'),
                                                capstyle='round')
         ax.add_collection(coll_pose)
-        return
-    if trajectory['position'] is not None:
-        ax.scatter(*trajectory['position'].T, c='k', s=5, edgecolor=(0, 0, 0, 0))
+
+    if get_value('plot_position') and trajectory['position'] is not None:
+        ax.scatter(*trajectory['position'][..., ::-1].T,
+                   marker=get_value('position_marker'),
+                   s=get_value('position_size'),
+                   facecolor=get_value('position_face'),
+                   edgecolor=get_value('position_edge'),
+                   linewidth=get_value('position_line_width'))
