2d4f0e4be953484b36dff21fc97f45a2aa14c322
pnuehrenberg <paul.nuehrenberg@uni.kn>
2025-02-18 12:47:53 +0100

    update cichlids dataset (int identifiers) and helper function

--- examples/social_cichlids/helpers.py
+++ examples/social_cichlids/helpers.py
@@ -9,67 +9,50 @@ def subsample_train(
     extractor,
     *,
     random_state=None,
-    exclude=None,
     log,
 ):
-    X_train_none, y_train_none = dataset.subsample(
+    X, y = dataset.subsample(
         extractor,
-        0.001,
-        categories=("none",),
-        try_even_subsampling=False,
+        {
+            ("approach", "chase", "dart_bite", "lateral_display", "quiver"): 1.0,
+            "frontal_display": 0.25,
+            "none": 0.001,
+        },
         random_state=random_state,
+        stratify=True,
+        reset_previous_indices=False,
+        exclude_previous_indices=False,
+        store_indices=False,
         log=log,
     )
-
-    X_frontal, y_frontal = dataset.subsample(
-        extractor,
-        0.25,
-        categories=("frontal_display",),
-        try_even_subsampling=False,
-        random_state=random_state,
-        log=log,
-    )
-
-    X_minorities, y_minorities = dataset.subsample(
-        extractor,
-        1.0,
-        categories=(
-            "approach",
-            "chase",
-            "dart_bite",
-            "lateral_display",
-            "quiver",
-        ),
-        try_even_subsampling=False,
-        random_state=random_state,
-        log=log,
-    )
-
-    # sample close neighbors more frequently
-    sampling_frequency = {0: 0.1, 1: 0.1, 2: 0.05, 3: 0.05, 4: 0.05}
-    X_additional = pd.concat(
-        [
-            permute_recipients(dataset, neighbor_rank=neighbor_rank).subsample(
-                extractor,
-                sampling_frequency[neighbor_rank],
-                categories=(
-                    "approach",
-                    "frontal_display",
-                    "chase",
-                    "dart_bite",
-                    "lateral_display",
-                    "quiver",
-                ),
-                try_even_subsampling=False,
-                random_state=random_state,
-                log=log,
-            )[0]  # only keep samples (X) but not labels (y)
-            for neighbor_rank in range(5)
-        ]
-    )
-    y_additional = np.repeat(
-        "none", len(X_additional)
-    )  # all labels are "none" because of switched recipients
-    X = pd.concat([X_train_none, X_frontal, X_minorities, X_additional])
-    y = np.concat([y_train_none, y_frontal, y_minorities, y_additional])
     return X, y
+
+    # # sample close neighbors more frequently
+    # sampling_frequency = {0: 0.1, 1: 0.1, 2: 0.05, 3: 0.05, 4: 0.05}
+    # X_additional = pd.concat(
+    #     [
+    #         permute_recipients(dataset, neighbor_rank=neighbor_rank).subsample(
+    #             extractor,
+    #             sampling_frequency[neighbor_rank],
+    #             categories=(
+    #                 "approach",
+    #                 "frontal_display",
+    #                 "chase",
+    #                 "dart_bite",
+    #                 "lateral_display",
+    #                 "quiver",
+    #             ),
+    #             try_even_subsampling=False,
+    #             random_state=random_state,
+    #             log=log,
+    #         )[0]  # only keep samples (X) but not labels (y)
+    #         for neighbor_rank in range(5)
+    #     ]
+    # )
+    # y_additional = np.repeat(
+    #     "none", len(X_additional)
+    # )  # all labels are "none" because of switched recipients
+    
+    # X = pd.concat([X_train_none, X_frontal, X_minorities])  # , X_additional
+    # y = np.concat([y_train_none, y_frontal, y_minorities])  # , y_additional
+    # return X, y
