0feddb41f69e22fdf09f417f1309dd243571fb21
Paul Nuehrenberg <paul.nuehrenberg@uni.kn>
2025-03-02 16:58:18 +0100

    add calms21 results

--- examples/CALMS21/run_postprocessing_experiment-mice-distributed.sh
+++ examples/CALMS21/run_postprocessing_experiment-mice-distributed.sh
@@ -9,7 +9,7 @@
 #SBATCH --nodes=20            # request as many nodes as iterations
 #SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
 #SBATCH --cpus-per-task=72    # assign all the cores to that first task to make room for Python's multiprocessing tasks
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 
 module purge
 module load gcc/10 impi/2021.2
