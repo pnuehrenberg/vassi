6a9274535a45ff1ad671f2d27b5c359e82172c59
Paul Nuehrenberg <paul.nuehrenberg@uni.kn>
2025-03-24 13:01:45 +0100

    add sequential optimization results (CALMS21)

---
+++ examples/CALMS21/extract_features-mice.py
@@ -0,0 +1,37 @@
+from automated_scoring.config import cfg
+from automated_scoring.features import DataFrameFeatureExtractor
+from automated_scoring.io import load_dataset
+
+cfg.key_keypoints = "keypoints"
+cfg.key_timestamp = "timestamps"
+
+cfg.trajectory_keys = (
+    "keypoints",
+    "timestamps",
+)
+
+if __name__ == "__main__":
+    dataset_train = load_dataset(
+        "mice_train",
+        directory="../../datasets/CALMS21/train",
+        target="dyad",
+        background_category="none",
+    )
+    dataset_test = load_dataset(
+        "mice_test",
+        directory="../../datasets/CALMS21/test",
+        target="dyad",
+        background_category="none",
+    )
+
+    extractor = DataFrameFeatureExtractor(
+        cache_directory="feature_cache_mice"
+    ).read_yaml("config_file.yaml")
+
+    for _, group in dataset_train.exclude_individuals(["intruder"]):
+        for _, sampleable in group:
+            sampleable.sample_X(extractor)
+
+    for _, group in dataset_test.exclude_individuals(["intruder"]):
+        for _, sampleable in group:
+            sampleable.sample_X(extractor)
