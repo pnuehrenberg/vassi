425ce5b6dcd390b583e35b6dcf7854ebb29bbfb7
pnuehrenberg <paul.nuehrenberg@uni.kn>
2025-04-01 15:12:47 +0200

    add cichlids sampling script

---
+++ examples/social_cichlids/run_sampling-cichlids.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#
+# Python multiprocessing example job script for MPCDF Raven.
+#
+#SBATCH -o ./out.%j
+#SBATCH -e ./err.%j
+#SBATCH -D ./
+#SBATCH -J PYTHON_MP
+#SBATCH --nodes=20            # request a single node
+#SBATCH --ntasks-per-node=1   # only start 1 task via srun because Python multiprocessing starts more tasks internally
+#SBATCH --cpus-per-task=72    # assign all the cores to that first task to make room for Python's multiprocessing tasks
+#SBATCH --time=02:00:00
+
+module purge
+module load gcc/10 impi/2021.2
+
+source activate autoscore
+
+srun python3 ./sampling-cichlids.py
---
+++ examples/social_cichlids/sampling-cichlids.py
@@ -0,0 +1,78 @@
+from helpers import subsample_train
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from automated_scoring.config import cfg
+from automated_scoring.features import DataFrameFeatureExtractor
+from automated_scoring.io import load_dataset, to_cache
+from automated_scoring.logging import set_logging_level
+from automated_scoring.sliding_metrics import (
+    SlidingWindowAggregator,
+    get_window_slices,
+    metrics,
+)
+
+cfg.key_keypoints = "pose"
+cfg.key_timestamp = "time_stamp"
+
+cfg.trajectory_keys = ("pose", "time_stamp")
+
+
+if __name__ == "__main__":
+    from automated_scoring.distributed import DistributedExperiment
+
+    dataset_full = load_dataset(
+        "cichlids",
+        directory="../../datasets/social_cichlids",
+        target="dyad",
+        background_category="none",
+    )
+
+    dataset_train, _ = dataset_full.split(
+        0.8,
+        random_state=1,
+    )
+
+    time_scales, slices = get_window_slices(3, time_scales=(91,))
+
+    aggregator = ColumnTransformer(
+        [
+            (
+                "aggregate",
+                SlidingWindowAggregator(
+                    [metrics.median, metrics.q10, metrics.q90], max(time_scales), slices
+                ),
+                make_column_selector(),
+            ),
+            ("original", "passthrough", make_column_selector()),
+        ],
+    )
+
+    pipeline = Pipeline(
+        [("impute", KNNImputer()), ("aggregate", aggregator)]
+    ).set_output(transform="pandas")
+
+    extractor = DataFrameFeatureExtractor(
+        cache_directory="cichlids_cache",
+        pipeline=pipeline,
+        refit_pipeline=True,
+        cache_mode="cached",
+    ).read_yaml("config_file-cichlids.yaml")
+
+    log = set_logging_level("info")
+
+    experiment = DistributedExperiment(20, random_state=1)
+    cache_directory = "samples_cache"
+
+    for run in experiment:
+        X, y = subsample_train(
+            dataset_train,
+            extractor,
+            random_state=experiment.random_state,
+            log=None,
+        )
+        y = dataset_train.encode(y)
+        to_cache(
+            (X, y), cache_file=f"samples_{run:02d}.cache", directory=cache_directory
+        )
