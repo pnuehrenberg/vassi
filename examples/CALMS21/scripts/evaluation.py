479682418a572f865d356d207fd2b0c6b6c03570
pnuehrenberg <paul.nuehrenberg@uni-konstanz.de>
2025-12-07 16:59:14 +0100

    fix evaluation script

--- examples/CALMS21/scripts/evaluation.py
+++ examples/CALMS21/scripts/evaluation.py
@@ -42,14 +42,14 @@ if __name__ == "__main__":
     env = Environment()
     dataset_train = AnnotatedDataset.load(
         "../../datasets/CALMS21/train/mice_train_trajectories.h5",
-        observation_file="../../datasets/CALMS21/train/mice_train_observations.csv",
+        observation_file="../../datasets/CALMS21/train/mice_train_annotations.csv",
         target="dyad",
         background_category="none",
     ).exclude({"intruder"})
 
     dataset_test = AnnotatedDataset.load(
         "../../datasets/CALMS21/test/mice_train_trajectories.h5",
-        observation_file="../../datasets/CALMS21/test/mice_test_observations.csv",
+        observation_file="../../datasets/CALMS21/test/mice_test_annotations.csv",
         target="dyad",
         background_category="none",
     ).exclude({"intruder"})
