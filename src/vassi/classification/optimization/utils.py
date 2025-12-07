93ce36c3ad65473e07eab8af9d9543892779d34a
pnuehrenberg <paul.nuehrenberg@uni-konstanz.de>
2025-12-07 21:43:11 +0100

    further debugging

--- src/vassi/classification/optimization/utils.py
+++ src/vassi/classification/optimization/utils.py
@@ -60,34 +60,28 @@ def is_aggregator_kwargs(kwargs: dict[str, object]) -> TypeGuard[AggregatorKwarg
         "keep_original_features",
     }
     if not required_keys.issubset(kwargs.keys()):
-        raise AssertionError("case a")
         return False
     if not isinstance(kwargs["keep_original_features"], bool):
-        raise AssertionError("case b")
         return False
     if kwargs["num_slices_per_window"] is not None and not isinstance(
         kwargs["num_slices_per_window"], int
     ):
-        raise AssertionError("case c")
         return False
     windows = kwargs["windows"]
     if not isinstance(windows, Iterable):
-        raise AssertionError("case d")
         return False
     if isinstance(windows, (list, tuple)):
+        # should instead use not isinstance(windows, Generator)
         if not all(isinstance(w, int) for w in windows):
-            raise AssertionError("case e")
             return False
     funcs = kwargs["sliding_metric_functions"]
     if not isinstance(funcs, Iterable):
-        raise AssertionError("case f")
         return False
     if not isinstance(funcs, Generator):
         # this is unlikely, but we should not consume values if its a generator
         if not all(isinstance(f, Callable) for f in funcs):
-            raise AssertionError("case g")
+            raise AssertionError(f"case g, got {[type(f) for f in funcs]}")
             return False
-    print("all good")
     return True
 
 
